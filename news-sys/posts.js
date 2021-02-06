f30c786107ccd29362c1e341f95d9ae6880d14b3
yeetree <64941543+yeetree@users.noreply.github.com>
2021-02-06 16:35:46 -0600

    Update posts.js

--- news-sys/posts.js
+++ news-sys/posts.js
@@ -1,2 +1,11 @@
+var newsDiv = document.getElementById("newsDiv");
+
+var newsSrc = "https://yeetree.github.io/ElectronBuilderSite/news-sys/";
+
 //a list of posts to be shown in the NEWS section
 var postList = ["2-6-2021.html", "2-6-2021-2.html];
+
+var i;
+for (i = 0; i <postList.length; i++) {
+  newsDiv.innerHTML += "<iframe src='" + newsSrc + postList[i] + "'</iframe>"
+}
